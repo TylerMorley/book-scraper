f381c285c43217fc4f71d3df4238047651dd5a17
Tyler Morley <TylerPMorley@gmail.com>
2017-07-18 08:09:33 -0500

    list of title and author pairs from front page

--- scraper.py
+++ scraper.py
@@ -12,12 +12,32 @@ bookData = tree.xpath(
 '//div[@class="wx_audiobook"]/p/a/em/text()'
 )
 
-print(bookData)
+authorData = tree.xpath('//div[@class="wx_audiobook"]/p/text()')
+cleanAuthorData = []
+for name in authorData:
+    if len(name) > 3:
+        begin = name.find("by") + 3
+        name = name[begin:]
+        comma = name.find(",")
+        if comma > 0:
+            name = name[:comma]
+        cleanAuthorData.append(name)
 
-expectedResult = ["The Savage Dead", "Plea",
+combinedData = []
+if len(bookData) == len(cleanAuthorData):
+    for index, val in enumerate(bookData):
+        print(index)
+        print(bookData[index])
+        print(cleanAuthorData[index])
+        entry = [bookData[index], cleanAuthorData[index]]
+        combinedData.append(entry)
+
+print(combinedData)
+
+"""expectedResult = ["The Savage Dead", "Plea",
 "Seventy Maxims of Maximally Effective Mercenaries",
 "Contracted Defense", "Vicious",
 "An Astronaut’s Guide to Life on Earth: What Going to Space Taught Me About " +
 "Ingenuity, Determination, and Being Prepared for Anything"
 ]
-print(expectedResult)
+print(expectedResult)"""
